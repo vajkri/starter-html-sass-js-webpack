f894dc1aa45691446799b98011637f9035fd1ac9
Kriszta <krisztavajda@gmail.com>
2021-07-02 20:43:38 +0200

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: false,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	proseWrap: 'always',
+	jsxBracketSameLine: true,
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	printWidth: 120,
+}
